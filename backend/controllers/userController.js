bd081ab6b38a745d31804bbb41e4d311a628e887
sreenadh34 <43.sreenadh@gmail.com>
2024-08-10 16:08:58 +0530

    Register User && Hash password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,22 +1,61 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+
 // @desc    Register new User
 // @route   POST /api/users
 // @access  Public
-const registerUser = ( req,res) => {
-    res.json({message: "Register User"})
-}
+const registerUser = asyncHandler(async ( req,res) => {
+    const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please add all fields');
+  }
+
+   // Check if user exists
+   const userExists = await User.findOne({ email });
+
+   if (userExists) {
+     res.status(400);
+     throw new Error('User already exists');
+   }
+     // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+      });
+    
+      if (user) {
+        res.status(201).json({
+          _id: user.id,
+          name: user.name,
+          email: user.email,
+        });
+      } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+      }
+})
 
 // @desc    Authenticate a user
 // @route   POST /api/users/login
 // @access  Public
-const loginUser = ( req,res) => {
+const loginUser = asyncHandler(( req,res) => {
     res.json({message: "login User"})
-}
+})
 
 // @desc    Get user data
 // @route   GET /api/users/me
 // @access  Private
-const getMe = ( req,res) => {
+const getMe = asyncHandler(( req,res) => {
     res.json({message: " User data"})
-}
+})
 
 export {registerUser, loginUser, getMe};
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -22,6 +22,6 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
